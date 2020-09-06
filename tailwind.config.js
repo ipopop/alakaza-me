c5181efd07d7b498e9819a53ffbf868538258871
ipopop <popop@ntymail.com>
2020-09-06 11:27:59 +0200

    lesson 4 - hover focus and active styles - ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
   plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    purge: {
+      layers: ['utilities'],
+      content: [
+        // Paths to your templates...
+      ],
+    },
+  },
 }
