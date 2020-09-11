70be357b389874ec86f6eb55fcfbfb965268b82d
ipopop <popop@ntymail.com>
2020-09-11 14:05:38 +0200

    chg repo name to alakaza-me

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-git push -f git@github.com:ipopop/my-tailwind-project.git master:gh-pages
+git push -f git@github.com:ipopop/alakaza-me.git master:gh-pages
 
 cd -
